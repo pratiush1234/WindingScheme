ed4d6be51513d8885a8eb173b8cac63637c55081
pratiush1234 <128517357+pratiush1234@users.noreply.github.com>
2023-03-23 12:59:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,12 +211,22 @@ if st.button("Show Analysis"):
         else:
             slotin1,slotout1,slotin2,slotout2,slotin3,slotout3 = func(number_of_phases,number_of_slots,number_of_poles)
 
-            df1 = pd.DataFrame(list(zip(slotin1, slotout1)),
-                        columns=['In', 'Out'],index = None)
-            df2 = pd.DataFrame(list(zip(slotin2, slotout2)),
-                        columns=['In', 'Out'],index = None)
-            df3 = pd.DataFrame(list(zip(slotin3, slotout3)),
-                        columns=['In', 'Out'],index = None)
+            # df1 = pd.DataFrame(list(zip(slotin1, slotout1)),
+            #             columns=['In', 'Out'],index = None)
+            # df2 = pd.DataFrame(list(zip(slotin2, slotout2)),
+            #             columns=['In', 'Out'],index = None)
+            # df3 = pd.DataFrame(list(zip(slotin3, slotout3)),
+            #             columns=['In', 'Out'],index = None)
+            arr = [n for n in range(1,int(number_of_slots)+1)]
+            arr1 = arr[:len(arr)//3]
+            arr2 = arr[len(arr)//3:2*len(arr)//3]
+            arr3 = arr[2*len(arr)//3:]
+            df1 = pd.DataFrame(list(zip(arr1,slotin1, slotout1)),
+               columns =['Coil Number','In', 'Out']).set_index("Coil Number")
+            df2 = pd.DataFrame(list(zip(arr2,slotin2, slotout2)),
+               columns =['Coil Number','In', 'Out']).set_index("Coil Number")
+            df3 = pd.DataFrame(list(zip(arr3,slotin3, slotout3)),
+               columns =['Coil Number','In', 'Out']).set_index("Coil Number")
             st.write('Winding Scheme for Phase A')
             st.table(df1)
             st.write('Winding Scheme for Phase B')
@@ -236,17 +246,83 @@ if st.button("Show Analysis"):
     else:
         st.write('Please give the valid input')
 
-st.write("To know more, give us a look [link](https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html)")
-st.write("Please visit our official website [link](https://www.iitg.ac.in/e_mobility/)")
+#####Footer
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
 
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
 
 
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
 
 
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 105px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=percent(8),
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height=percent(10),
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(4, 4, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
 
 
+def footer():
+    myargs = [
+        "Visit Our Websites   ",
+        link("https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html", " || Project Website|| "),
+        "      ",
+        link("https://www.iitg.ac.in/e_mobility/", "|| Lab Website|| "),
+    ]
+    layout(*myargs)
 
 
+if __name__ == "__main__":
+    footer()
 
-
+#st.write("To know more, give us a look [link](https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html)")
+#st.write("Please visit our official website [link](https://www.iitg.ac.in/e_mobility/)")
