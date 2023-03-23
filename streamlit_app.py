f05f767f4e69e4000c561f4ec61f6ec26bbb954d
pratiush1234 <128517357+pratiush1234@users.noreply.github.com>
2023-03-23 14:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,6 +171,28 @@ def CheckForFullPitchedWinding(number_of_phases,number_of_slots,number_of_poles)
         #return "Winding is Short Pitched"
     return flag,slot_pitch_mech,slot_pitch_elec,coil_pitch_mech,coil_pitch_elec
 
+def misc_parameter(number_of_slots,number_of_poles):
+    number_of_slots = int(number_of_slots)
+    number_of_poles = int(number_of_poles)
+    # Step 4: Define variables
+    slot_pitch_mech = 360 / number_of_slots
+    slot_pitch_elec = (number_of_poles / 2) * slot_pitch_mech
+    coil_span = int(number_of_slots / number_of_poles)
+    coil_pitch = coil_span * slot_pitch_elec
+    # calculation for pitch factor // give angles in radians 
+    
+    chording_angle = (180 - coil_pitch) / 2
+    number_of_coils = number_of_slots*2
+
+    pitch_factor = math.cos((math.pi/180)*chording_angle/2)
+
+    # angular displacement between slots
+    beta = (180*number_of_poles)/number_of_slots
+
+    # calculation of distribution factor
+    distribution_factor = math.sin((math.pi/180)*0.5*beta*number_of_slots_per_pole_per_phase)/(number_of_slots_per_pole_per_phase*math.sin(0.5*beta))
+    return pitch_factor, distribution_factor, pitch_factor*distribution_factor
+
 number_of_phases = 3
 st.set_page_config(page_title="Winding Scheme")
 page_bg_img = """
@@ -210,13 +232,6 @@ if st.button("Show Analysis"):
             st.header('Double layer winding is not feasible for the given number of poles and slots combination.')
         else:
             slotin1,slotout1,slotin2,slotout2,slotin3,slotout3 = func(number_of_phases,number_of_slots,number_of_poles)
-
-            # df1 = pd.DataFrame(list(zip(slotin1, slotout1)),
-            #             columns=['In', 'Out'],index = None)
-            # df2 = pd.DataFrame(list(zip(slotin2, slotout2)),
-            #             columns=['In', 'Out'],index = None)
-            # df3 = pd.DataFrame(list(zip(slotin3, slotout3)),
-            #             columns=['In', 'Out'],index = None)
             arr = [n for n in range(1,int(number_of_slots)+1)]
             arr1 = arr[:len(arr)//3]
             arr2 = arr[len(arr)//3:2*len(arr)//3]
@@ -240,89 +255,17 @@ if st.button("Show Analysis"):
             elif flag1 == 0:
                 st.write('Winding is Short Pitched')
 
+            pitch_factor,distribution_factor,winding_factor = misc_parameter(number_of_slots,number_of_poles)
+
             st.write('Coil pitch in number of slots or Coil Span:', slot_pitch_mech)
+            st.write('Pitch Factor:',pitch_factor)
+            st.write('Distribution Factor:',distribution_factor)
+            st.write('Winding Factor:',winding_factor)
             st.write('Coil pitch in electrical degrees: ', coil_pitch_elec)
             st.write('Coil pitch in mechanical degrees: ', coil_pitch_mech)
     else:
         st.write('Please give the valid input')
 
-# #####Footer
-# from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-# from htbuilder.units import percent, px
-# from htbuilder.funcs import rgba, rgb
-
-
-# def image(src_as_string, **style):
-#     return img(src=src_as_string, style=styles(**style))
-
-
-# def link(link, text, **style):
-#     return a(_href=link, _target="_blank", style=styles(**style))(text)
-
-
-# def layout(*args):
-
-#     style = """
-#     <style>
-#       # MainMenu {visibility: hidden;}
-#       footer {visibility: hidden;}
-#      .stApp { bottom: 105px; }
-#     </style>
-#     """
-
-#     style_div = styles(
-#         position="fixed",
-#         left=0,
-#         bottom=percent(8),
-#         margin=px(0, 0, 0, 0),
-#         width=percent(100),
-#         color="black",
-#         text_align="center",
-#         height=percent(10),
-#         opacity=1
-#     )
-
-#     style_hr = styles(
-#         display="block",
-#         margin=px(4, 4, "auto", "auto"),
-#         border_style="inset",
-#         border_width=px(2)
-#     )
-
-#     body = p()
-#     foot = div(
-#         style=style_div
-#     )(
-#         hr(
-#             style=style_hr
-#         ),
-#         body
-#     )
-
-#     st.markdown(style, unsafe_allow_html=True)
-
-#     for arg in args:
-#         if isinstance(arg, str):
-#             body(arg)
-
-#         elif isinstance(arg, HtmlElement):
-#             body(arg)
-
-#     st.markdown(str(foot), unsafe_allow_html=True)
-
-
-# def footer():
-#     myargs = [
-#         "Visit Our Websites   ",
-#         link("https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html", " || Project Website|| "),
-#         "      ",
-#         link("https://www.iitg.ac.in/e_mobility/", "|| Lab Website|| "),
-#     ]
-#     layout(*myargs)
-
-
-# if __name__ == "__main__":
-#     footer()
 
 st.write("To know more, give us a look [link](https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html)")
 st.write("Please visit our official website [link](https://www.iitg.ac.in/e_mobility/)")
