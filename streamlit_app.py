59165637a7a4406b6ceb2d737df8657af6803cb3
pratiush1234 <128517357+pratiush1234@users.noreply.github.com>
2023-04-06 16:33:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import math
 import helper
+import emf_polygon
+import matplotlib.pyplot as plt
 
 number_of_phases = 3
 st.set_page_config(page_title="Winding Scheme")
@@ -43,7 +45,7 @@ if option == 'Double Layer Winding':
                 st.markdown(" :red[Double layer winding is not feasible for the given number of poles and slots combination.]")
                 #st.header('Double layer winding is not feasible for the given number of poles and slots combination.')
             else:
-                slotin1,slotout1,slotin2,slotout2,slotin3,slotout3 = helper.double_layer_func(number_of_phases,number_of_slots,number_of_poles)
+                slotin1,slotout1,slotin2,slotout2,slotin3,slotout3,theta_angle = helper.double_layer_func(number_of_phases,number_of_slots,number_of_poles)
                 arr = [n for n in range(1,int(number_of_slots)+1)]
                 arr1 = arr[:len(arr)//3]
                 arr2 = arr[len(arr)//3:2*len(arr)//3]
@@ -69,7 +71,7 @@ if option == 'Double Layer Winding':
                     st.write('Winding is Short Pitched')
                     st.write('You can choose any positive integer value lesser than',coil_span_in_slot_pitch,'(if exists)')
 
-                pitch_factor,distribution_factor,winding_factor = helper.double_layer_misc_parameter(number_of_slots,number_of_poles)
+                pitch_factor, distribution_factor, winding_factor = helper.double_layer_misc_parameter(number_of_slots,number_of_poles)
 
                 #st.write('Coil pitch in number of slots or Coil Span:', slot_pitch_mech)
                 st.write('Pitch Factor:',pitch_factor)
@@ -77,6 +79,44 @@ if option == 'Double Layer Winding':
                 st.write('Winding Factor:',winding_factor)
                 st.write('Coil pitch in electrical degrees: ', coil_pitch_elec)
                 st.write('Coil pitch in mechanical degrees: ', coil_pitch_mech)
+
+                phasors, magnitude, angle, phasor_sum = emf_polygon.emf_polygon(theta_angle)
+                st.write('Magnitude:', magnitude)
+                st.write('Angle:',angle)
+                fig, ax = plt.subplots(figsize=(8, 8))
+
+                # Initialize the starting point of the polygon at the origin
+                x_start, y_start = 0, 0
+
+                # Loop through the list of phasors and plot them as vectors in a cartesian coordinate system
+                for phasor in phasors:
+                    # Get the real and imaginary components of the phasor
+                    x = phasor.real
+                    y = phasor.imag
+
+                    # Plot the phasor as a vector
+                    ax.quiver(x_start, y_start, x, y, angles='xy', scale_units='xy', scale=1, color='blue')
+
+                    # Update the starting point of the polygon
+                    x_start += x
+                    y_start += y
+
+                # Plot the resultant vector as a red vector
+                ax.quiver(0, 0, phasor_sum.real, phasor_sum.imag, angles='xy', scale_units='xy', scale=1, color='red')
+
+                # Set the x and y limits of the plot
+                ax.set_xlim([-5, 5])
+                ax.set_ylim([-5, 5])
+
+                # Set the labels for the x and y axes
+                ax.set_xlabel('Real axis')
+                ax.set_ylabel('Imaginary axis')
+
+                # Add a title to the plot
+                ax.set_title('Vector Polygon')
+                st.pyplot(fig)
+
+
         else:
             st.write('Please give the valid input')
 
@@ -92,7 +132,7 @@ elif(option == 'Single Layer Winding'):
             #st.write('Coil Offset:',coil_offset)
             #st.write('Number of slots per pole per phase:',number_of_slots_per_pole_per_phase)
             if flag == 1:
-                st.markdown(" :red[SIngle layer winding is not feasible for the given number of poles and slots combination.]")
+                st.markdown(" :red[Single layer winding is not feasible for the given number of poles and slots combination.]")
                 #st.header('Double layer winding is not feasible for the given number of poles and slots combination.')
             else:
                 slotin1,slotout1,slotin2,slotout2,slotin3,slotout3 = helper.single_layer_func(number_of_slots,number_of_poles)
@@ -115,15 +155,9 @@ elif(option == 'Single Layer Winding'):
 
 
 
+            
+
+
+
 st.write("To know more, give us a look [link](https://c-tarac.github.io/AI-ML-Based-Motor-Design.github.io/index.html)")
 st.write("Please visit our official website [link](https://www.iitg.ac.in/e_mobility/)")
-
-
-
-
-
-
-
-
-
-
