eb99634596ca7cee6f5c65f2082f25c0cae624b2
pratiush1234 <128517357+pratiush1234@users.noreply.github.com>
2023-04-08 16:11:37 +0530

    Update emf_polygon.py

--- emf_polygon.py
+++ emf_polygon.py
@@ -1,32 +1,40 @@
-from collections import Counter
-import cmath
+
 import math
-import matplotlib.pyplot as plt
+import cmath
+from collections import Counter
 
 def emf_polygon(phase_angle):
-    phasor_dict = Counter(phase_angle)
-    # Initialize the phasor sum
+
+    phasor_dict = {}
+    for i, val in enumerate(phase_angle):
+        if val in phasor_dict:
+            phasor_dict[val][0] += 1
+            phasor_dict[val][1].append(i)
+        else:
+            phasor_dict[val] = [1, [i]]
+
     phasor_sum = 0
+    index_list = []
+    for key, value in phasor_dict.items():
+        index_list.append(value[1])
+        
 
     for key, value in phasor_dict.items():
-        if value%2 == 0 and phase_angle.count(key) > 1:
-            phasor_dict[key] = 1
+        if value[0]%2 == 0 and phase_angle.count(key) > 1:
+            phasor_dict[key][0] = 1
 
-        elif value%2 == 1 and phase_angle.count(key) > 1:
-            phasor_dict[key] = 2
+        elif value[0]%2 == 1 and phase_angle.count(key) > 1:
+            phasor_dict[key][0] = 2
     phasor_sum = 0
     phasors = []
 
-    # Loop through the dictionary and add each phasor to the list of phasors
     for angle, magnitude in phasor_dict.items():
-        phasor = cmath.rect(magnitude, math.radians(angle))
+        phasor = cmath.rect(magnitude[0], math.radians(angle))
         phasors.append(phasor)
 
-    # Compute the phasor sum
     phasor_sum = sum(phasors)
 
-    # Get the magnitude and angle of the phasor sum
     magnitude = abs(phasor_sum)
     angle = cmath.phase(phasor_sum)
 
-    return phasors, magnitude, angle, phasor_sum
+    return phasors, round(magnitude,3), angle,phasor_sum,index_list
